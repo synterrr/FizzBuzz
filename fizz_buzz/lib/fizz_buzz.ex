a59c711a6223ad57d7a5298dc1db467da85b947e
SynTer <alpacanismo@gmail.com>
2021-03-08 21:15:22 -0300

    part of it done, replacing already fizz and buzz

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -6,8 +6,26 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
+  #def handle_file_read({:ok, result}) do
+  #  list = String.split(result, ",")
+  #  Enum.map(list, &String.to_integer/1)
+  #end
   def handle_file_read({:ok, result}) do
-    list = String.split(result, ",")
+    result
+    |> String.split(",")
+    |> Enum.map(&convert_and_evaluate_number/1)
   end
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  def convert_and_evaluate_number(elem) do
+    number = String.to_integer(elem)
+
+    if rem(number, 3) == 0 do
+      :fizz
+    else if (rem(number, 5) == 0) do
+         :buzz
+         end
+    end
+  end
+
 end
