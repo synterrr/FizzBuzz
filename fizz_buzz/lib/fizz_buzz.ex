cbce9a5896593e74f650f98bbd2dd73bb27e581f
SynTer <alpacanismo@gmail.com>
2021-03-10 19:02:16 -0300

    Now fizzbuzz, fizz and buzz it's working

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -10,22 +10,25 @@ defmodule FizzBuzz do
   #  list = String.split(result, ",")
   #  Enum.map(list, &String.to_integer/1)
   #end
-  def handle_file_read({:ok, result}) do
-    result
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
     |> String.split(",")
     |> Enum.map(&convert_and_evaluate_number/1)
-  end
-  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 
-  def convert_and_evaluate_number(elem) do
-    number = String.to_integer(elem)
-
-    if rem(number, 3) == 0 do
-      :fizz
-    else if (rem(number, 5) == 0) do
-         :buzz
-         end
-    end
+    {:ok, result}
   end
 
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+  defp convert_and_evaluate_number(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
