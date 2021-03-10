ea9074a7cd3a31b22dac988d486862a709cc5e6a
SynTer <alpacanismo@gmail.com>
2021-03-10 20:04:12 -0300

    created the ExUnit case test

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,20 +1,19 @@
 defmodule FizzBuzz do
-
   def build(file_name) do
     file_name
     |> File.read()
     |> handle_file_read()
   end
 
-  #def handle_file_read({:ok, result}) do
+  # def handle_file_read({:ok, result}) do
   #  list = String.split(result, ",")
   #  Enum.map(list, &String.to_integer/1)
-  #end
+  # end
   defp handle_file_read({:ok, result}) do
     result =
       result
-    |> String.split(",")
-    |> Enum.map(&convert_and_evaluate_number/1)
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_number/1)
 
     {:ok, result}
   end
